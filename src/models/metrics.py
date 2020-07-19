a0f16c44524a9cdb89db887e0043b2e5bf734c5e
Danilo de Oliveira <ors.danilo@gmail.com>
2020-07-19 16:33:55 -0300

    Add comparison to Scikit Learn's Linear Regressor

--- src/models/metrics.py
+++ src/models/metrics.py
@@ -1,29 +1,29 @@
 import numpy as np
 import math
 
-def r2_score(y, y_pred):
+def r2_score(y_true, y_pred):
     """Calculate the coefficient of determination"""
 
     # ybar = numpy.sum(y)/len(y) 
-    ssreg = np.sum((y_pred - y.mean()) ** 2)
-    sstot = np.sum((y - y.mean())**2) 
+    ssreg = np.sum((y_pred - y_true.mean()) ** 2)
+    sstot = np.sum((y_true - y_true.mean()) ** 2) 
     r2 = ssreg / sstot
     return r2
 
-def rmse_score(y, y_pred):
+def rmse_score(y_true, y_pred):
     """Calculate the root mean squared error"""
 
-    rmse = math.sqrt(np.sum(y_pred - y) ** 2 / y.size)
+    rmse = math.sqrt(np.sum(y_pred - y_true) ** 2 / y_true.size)
     return rmse
 
-def mse_score(y, y_pred):
+def mse_score(y_true, y_pred):
     """Calculate the mean squared error"""
 
-    mse = np.sum(y_pred - y) ** 2 / y.size
+    mse = np.sum(y_pred - y_true) ** 2 / y_true.size
     return mse
 
-def mae_score(y, y_pred):
+def mae_score(y_true, y_pred):
     """Calculate the mean absolute error"""
 
-    mae = np.sum(np.abs(y_pred - y) ** 2) / y.size
+    mae = np.sum(np.abs(y_pred - y_true) ** 2) / y_true.size
     return mae
