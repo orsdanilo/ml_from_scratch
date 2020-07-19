17eb658f054328ffbca59482a6dc3eb2d7f56ce2
Danilo de Oliveira <ors.danilo@gmail.com>
2020-07-18 21:55:14 -0300

    Add docstrings to _linear.py

--- src/models/_linear.py
+++ src/models/_linear.py
@@ -5,7 +5,9 @@ import matplotlib.pyplot as plt
 from .metrics import mse_score, rmse_score, r2_score
 
 class LinearRegressor():
-    def __init__(self, method='normal_equation', lr = 0.01, epochs = 1000):
+    """Linear regressor"""
+
+    def __init__(self, method='normal_equation', lr=0.01, epochs=1000):
         assert method in ['normal_equation', 'gradient_descent'], "Method not supported. Supported methods are 'normal_equation' and 'gradient_descent'"
         self.method = method
         if self.method == 'gradient_descent':
@@ -15,40 +17,49 @@ class LinearRegressor():
             pass
     
     def fit(self, X, y):
+        """Fit the model to the data"""
+
         X = X.to_numpy()
         y = y.to_numpy()
 
         if self.method == 'normal_equation':
             self._weights = np.dot(np.dot(np.linalg.inv(np.dot(X.T, X)), X.T), y)
-            
+
         else:
             # mse_new = np.inf
             self._weights = np.zeros(X.shape[1])
-            self.cost_hist = [0] * self.epochs
+            self.cost_history = [0] * self.epochs
             m = len(y)
 
             for i in range(self.epochs):
                 self._weights = self._weights - (self.lr/m) * np.dot(X.T, np.dot(X, self._weights) - y)
-                self.cost_hist[i] = mse_score(y, np.dot(X, self._weights))
+                self.cost_history[i] = mse_score(y, np.dot(X, self._weights))
 
             #     if (rmse_new > rmse_old):
             #         print("Stopped at iteration {}".format(i))
             #         break
-            plt.scatter(range(self.epochs), self.cost_hist)
+            plt.scatter(range(self.epochs), self.cost_history)
             plt.xlabel('epoch')
             plt.ylabel('mse')
 
     def predict(self, X):
+        """Use the fitted model to predict on data"""
+
+        assert self._weights, "Model needs to be fitted first. Use the fit method"
         X = X.to_numpy()
         y = np.dot(X, self._weights)
         return y
     
     def get_weights(self):
+        """Get weights from the fitted model"""
+
+        assert self._weights, "Model needs to be fitted first. Use the fit method"
         return self._weights
 
-    def score(self, X, y, metric = 'r2'):
+    def score(self, X, y, metric='r2'):
+        """Score the model"""
 
-        assert metric in ['r2', 'rmse'], "Metric not supported. Supported metrics are 'r2' and 'rmse'"
+        assert metric in ['r2', 'rmse'], "Metric not supported. Supported metrics are 'r2', 'mae' and 'rmse'"
         y_pred = self.predict(X)
         if metric == 'r2':
             score = r2_score(y, y_pred)
