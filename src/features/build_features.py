adc504cd3f8e2c4b40c793ff368c9f9a7a296550
Danilo de Oliveira <ors.danilo@gmail.com>
2020-07-19 11:27:39 -0300

    Add scaler, change regression data

--- src/features/build_features.py
+++ src/features/build_features.py
@@ -13,8 +13,59 @@ def train_test_split(X, y, test_size=0.2):
     y_test = y[~msk]
     return X_train, y_train, X_test, y_test
 
-def standardize_data(df):
-    """Standardize columns of the dataframe, returning a centered dataframe"""
+class StandardScaler():
+    """Scaler for data standardization"""
 
-    df_norm = (df - df.mean()) / (df.std())
-    return df_norm
+    def __init__(self):
+        pass
+
+    def fit(self, df):
+        """Fit the scaler to the data"""
+
+        self.mean = df.mean()
+        self.std = df.std()
+
+    def transform(self, df):
+        """Standardize columns of the dataframe, returning a centered dataframe"""
+
+        return (df - self.mean) / self.std
+
+    def fit_transform(self, df):
+        """Fit the scaler to the data and return the data standardized"""
+        
+        self.fit(df)
+        return self.transform(df)
+
+    def inverse_transform(self, df):
+        """"Rescale standardized data to its original form"""
+
+        return df * self.std + self.mean
+
+class MinMaxScaler():
+    """Scaler for data normalization"""
+
+    def __init__(self):
+        pass
+
+    def fit(self, df):
+        """Fit the scaler to the data"""
+
+        self.min = df.min()
+        self.max = df.max()
+
+    def transform(self, df):
+        """Standardize columns of the dataframe, returning a centered dataframe"""
+
+        return (df - self.min) / (self.max - self.min)
+
+    def fit_transform(self, df):
+        """Fit the scaler to the data and return the data standardized"""
+        
+        self.fit(df)
+        return self.transform(df)
+
+    def inverse_transform(self, df):
+        """"Rescale standardized data to its original form"""
+
+        return df * (self.max - self.min) + self.min
+
--- src/models/_linear.py
+++ src/models/_linear.py
@@ -2,22 +2,32 @@ import pandas as pd
 import numpy as np
 import math
 import matplotlib.pyplot as plt
-from .metrics import mse_score, rmse_score, r2_score
+from .metrics import mse_score, rmse_score, r2_score, mae_score
+from ..features.build_features import StandardScaler, MinMaxScaler
 
 class LinearRegressor():
     """Linear regressor"""
 
-    def __init__(self, method='normal_equation', lr=0.01, epochs=1000):
+    def __init__(self, method='normal_equation', normalize=False, lr=0.01, epochs=1000):
         assert method in ['normal_equation', 'gradient_descent'], "Method not supported. Supported methods are 'normal_equation' and 'gradient_descent'"
         self.method = method
+        self.normalize = normalize
+        self._weights = None
+
         if self.method == 'gradient_descent':
             self.lr = lr
             self.epochs = epochs
-        else:
-            pass
+
+        if self.normalize:
+            self._feature_scaler = MinMaxScaler()
+            self._target_scaler = MinMaxScaler()
     
     def fit(self, X, y):
         """Fit the model to the data"""
+        
+        if self.normalize:
+            X = self._feature_scaler.fit_transform(X)
+            y = self._target_scaler.fit_transform(y)
 
         X = X.to_numpy()
         y = y.to_numpy()
@@ -45,22 +55,32 @@ class LinearRegressor():
     def predict(self, X):
         """Use the fitted model to predict on data"""
 
-        assert self._weights, "Model needs to be fitted first. Use the fit method"
+        assert self._weights is not None, "Model needs to be fitted first. Use the fit method"
+
+        if self.normalize:
+            X = self._feature_scaler.transform(X)
+
         X = X.to_numpy()
-        y = np.dot(X, self._weights)
-        return y
+        y_pred = np.dot(X, self._weights)
+
+        if self.normalize:
+            y_pred = self._target_scaler.inverse_transform(y_pred)
+
+        return np.round(y_pred, 2)
     
     def get_weights(self):
         """Get weights from the fitted model"""
 
-        assert self._weights, "Model needs to be fitted first. Use the fit method"
+        assert self._weights is not None, "Model needs to be fitted first. Use the fit method"
         return self._weights
 
     def score(self, X, y, metric='r2'):
         """Score the model"""
 
         assert metric in ['r2', 'rmse'], "Metric not supported. Supported metrics are 'r2', 'mae' and 'rmse'"
-        y_pred = self.predict(X)
+
+        y_pred = self.predict(X)    
+        
         if metric == 'r2':
             score = r2_score(y, y_pred)
         elif metric == 'rmse':
